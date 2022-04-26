e28d49574444db81fcc1fbaeb38dc3df56840e00
jaweherCherif <36386803+jaweherCherif@users.noreply.github.com>
2022-04-26 14:16:48 +0200

    finish mission 1

--- index.js
+++ index.js
@@ -1,88 +1,127 @@
 import fetch from "node-fetch";
 
-const authenticate = async (authData) => {
+const login = async (authData) => {
   let auth =
     "Basic " +
     Buffer.from(authData.client_id + ":" + authData.client_secret).toString(
       "base64"
     );
-  const refreshToken = await fetch("http://localhost:3000/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: auth,
-    },
-    body: JSON.stringify({
-      user: authData.user,
-      password: authData.password,
-    }),
-  })
-    .then((res) => res.json())
-    .then((json) => json)
-    .catch((err) => console.log(err));
-  console.log(refreshToken);
-  return refreshToken;
+  try {
+    const res = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: auth,
+      },
+      body: JSON.stringify({
+        user: authData.user,
+        password: authData.password,
+      }),
+    });
+    return res.status == 200 ? res.json() : res;
+  } catch (err) {
+    return err;
+  }
 };
 
-const getToken = (refreshToken) => {
-  return fetch("http://localhost:3000/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      grant_type: "refresh_token",
-      refresh_token: refreshToken,
-    }),
-  })
-    .then((res) => res.json())
-    .then((json) => json)
-    .catch((err) => console.log(err));
+const getToken = async (refreshToken) => {
+  try {
+    const res = await fetch("http://localhost:3000/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+      }),
+    });
+    return res.status == 200 ? res.json() : res;
+  } catch (err) {
+    return err;
+  }
 };
 const getAccounts = async (token) => {
-  return fetch("http://localhost:3000/accounts", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => json)
-    .catch((err) => console.log(err));
+  try {
+    const res = await fetch("http://localhost:3000/accounts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.status == 200 ? res.json() : res;
+  } catch (err) {
+    return err;
+  }
 };
 
 const getTransactions = async (token, account) => {
-  fetch(`http://localhost:3000/accounts/${account}/transaction`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => json)
-    .catch((err) => console.log(err));
+  try {
+    const res = await fetch(
+      `http://localhost:3000/accounts/${account}/transactions`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return res;
+  } catch (err) {
+    return err;
+  }
 };
 
-const refreshToken = authenticate({
+const loginInfo = {
   user: "BankinUser",
   password: "12345678",
   client_id: "BankinClientId",
   client_secret: "secret",
-}).then((data) => {
-  if (data.refresh_token) {
-    getToken(data.refresh_token).then((data) => {
-      const token = data.access_token;
-      if (token) {
-        getAccounts(token).then((accounts) => {
-          accounts.account.map((account) =>
-            getTransactions(token, account.acc_number).then((transaction) =>
-              console.log(transaction)
-            )
-          );
-        });
-      }
-    });
+};
+
+const getAccountsFullData = async () => {
+  try {
+    const loginRes = await login(loginInfo);
+    const refreshToken = loginRes.refresh_token;
+    if (!refreshToken) return { status: "not authorized", accounts: [] };
+    let accountsFullData = [];
+    const tokenRes = await getToken(refreshToken);
+    if (!tokenRes.access_token)
+      return { status: "refresh token is worng", accounts: [] };
+    const token = tokenRes.access_token;
+    const accountsRes = await getAccounts(token);
+    if (!accountsRes.account)
+      return { status: "access token is wrong", accounts: [] };
+    await Promise.all(
+      accountsRes.account.map(async (account) => {
+        let accountFullData = {
+          acc_number: account.acc_number,
+          amount: account.amount,
+        };
+        const transactions = await getTransactions(token, account.acc_number);
+        if (transactions.status !== 200)
+          accountFullData = { ...accountFullData, transactions: [] };
+        else {
+          const transactionList = await transactions.json();
+          accountFullData = {
+            ...accountFullData,
+            transactions: transactionList.transactions.map((transaction) => ({
+              label: transaction.label,
+              amount: transaction.amount,
+              currency: transaction.currency,
+            })),
+          };
+        }
+        accountsFullData.push(accountFullData);
+      })
+    );
+    return { status: "authorized", accounts: accountsFullData };
+  } catch (err) {
+    console.log("err", err);
+    return err;
   }
-});
+};
+
+getAccountsFullData().then((result) => console.log(result.accounts));
